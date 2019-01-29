1c90b0dd35ad8cd354675a60ab05cb064940b1b8
Bogdan5 <bogdan5x@gmail.com>
2019-01-29 16:42:20 -0500

    Routes  linted

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,5 @@
 const express = require('express');
+
 const router = express.Router();
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
@@ -10,3 +11,36 @@ const validateLoginInput = require('../validation/login');
 
 // Load User model
 const User = require('../models/User');
+
+// @route POST api/users/register
+// @desc Register user
+// @access Public
+router.post('/register', (req, res) => {
+  // Form validation
+  const { errors, isValid } = validateRegisterInput(req.body);
+  // Check validation
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+  User.findOne({ email: req.body.email }).then((user) => {
+    if (user) {
+      return res.status(400).json({ email: 'Email already exists' });
+    }
+    const newUser = new User({
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
+    });
+    // Hash password before saving in database
+    bcrypt.genSalt(10, (err, salt) => {
+      bcrypt.hash(newUser.password, salt, (error, hash) => {
+        if (err) throw err;
+        newUser.password = hash;
+        newUser
+          .save()
+          .then(user1 => res.json(user1))
+          .catch(err1 => console.log(err1));
+      });
+    });
+  });
+});
