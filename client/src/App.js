5d3d19d7f719ddccbc7ae7495799c81d5b4d1420
Bogdan5 <bogdan5x@gmail.com>
2019-02-03 22:14:22 -0500

    App.js state and event handlers 1

--- client/src/App.js
+++ client/src/App.js
@@ -16,3 +16,44 @@ import ColumnSelector from './components/ColumnSelector';
 
 import './App.css';
 import SortButton from './components/SortButton';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.textInput = React.createRef();
+    this.appRef = React.createRef();
+    this.state = {
+      listCards: [{ // list of all the conditions cards that include conditional buttons
+        id: 0, // id to identify each card
+        field: 'all', // on what columns of the data the list of operations apply
+        listElements: [],
+        listOperations: [], // what conditions aply to the data - helps sort
+      }],
+      cardSelected: 0, // card that is currently selected to work on
+      keyword: '', // content of the keyword input text field
+      inputVisibility: 'hidden', // in the second Keyboard, whether the position input is visible
+      keywordButtonClicked: '', // name of button clicked in the keyword(2nd) Keyboard
+      active: [], // array of buttons active
+      position: 0,
+      idConditional: 0,
+      menuVisible: false,
+      mergerArray: [null, null, null],
+      data: [{
+        column1: 12,
+        column2: 23,
+        column3: 20,
+      }],
+    };
+  }
+
+  componentDidMount() {
+    this.textInput.current.focus();
+  }
+
+  textHandler = (e) => {
+    this.setState({ keyword: e.target.value });
+  };
+
+  positionHandler = (e) => {
+    this.setState({ position: e.target.value });
+  }
