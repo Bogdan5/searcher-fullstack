f75a3e17094e46d11d34475c953e512ff796fd65
Bogdan5 <bogdan5x@gmail.com>
2019-02-03 22:13:18 -0500

    Imports in app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react';
+import Keyboard from './components/Keyboard';
+import ButtonGroup from './components/ButtonGroup';
+import DataDisplay from './components/DataDisplay';
+import DumbButton from './components/DumbButton';
+import Header from './components/Header';
+import Sorter from './components/Sorter';
+import ComponentEnhancer from './components/ComponentEnhancer';
+import ConditionButtonFormatter from './components/ConditionButtonFormatter';
+import ConditionButton from './components/ConditionButton';
+import DropDownMenu from './components/DropDownMenu';
+import MenuOption from './components/MenuOption';
+import Icon from './components/Icon';
+import SelectButton from './components/SelectButton';
+import ColumnSelector from './components/ColumnSelector';
+
 import './App.css';
-
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>Hello!</h1>
-      </div>
-    );
-  }
-}
-
-export default App;
+import SortButton from './components/SortButton';
