b8454e61551ece3dd42640800ac18361f6c4888c
Bogdan5 <bogdan5x@gmail.com>
2019-02-09 20:52:36 -0500

    Keyboard

---
+++ client/src/components/Keyboard.js
@@ -0,0 +1,22 @@
+import React from 'react';
+// import PropTypes from 'prop-types';
+import '../App.css';
+
+// this component includes a description on the left and to the right a set of\
+// buttons used to construct the query
+const Keyboard = (props) => {
+  const { classProp, leftSection, rightSection, cardSelected, id } = props;
+  const { children } = props;
+  const isSelected = (id || id === 0) && (cardSelected === id) ? ' selectedKeyboard' : '';
+  return (
+    <div className={`keyboardGeneric${classProp}`}>
+      <div className={`leftSection${isSelected}`}>{leftSection}</div>
+      <div className={isSelected}>{children}</div>
+      <section>{rightSection}</section>
+    </div>
+  );
+};
+
+// Keyboard.propTypes = { typeContent: PropTypes.string.isRequired };
+
+export default Keyboard;
