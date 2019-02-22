a6e3a43cc68efe04effa54f81393d12a6fa54b0c
Bogdan5 <bogdan5x@gmail.com>
2019-02-21 23:30:40 -0500

    Upload multer ending

--- server.js
+++ server.js
@@ -19,10 +19,23 @@ const server = http.createServer(app);
 const port = 9000;
 
 router.post('/', upload.single('file'), function (req, res) {
+  const fileRows = [];
+
+  // open uploaded file
+  csv.fromPath(req.file.path)
+    .on("data", function (data) {
+      fileRows.push(data); // push each row
+    })
+    .on("end", function () {
+      console.log(fileRows)
+      fs.unlinkSync(req.file.path);   // remove temp file
+      //process "fileRows" and respond
+    })
 });
 
 app.use('/upload-csv', router);
 
+// Start server
 function startServer() {
   server.listen(port, function () {
     console.log('Express server listening on ', port);
@@ -34,35 +47,35 @@ setImmediate(startServer);
 
 
 
-// Bodyparser middleware
-app.use(
-  bodyParser.urlencoded({
-    extended: false,
-  }),
-);
-app.use(multer({ dest: './uploads' }).any());
-// app.use(fileUpload());
-app.use(bodyParser.json());
-// DB Config
-const db = process.env.MONGODB_URI;
-console.log(db);
-// Connect to MongoDB
-mongoose
-  .connect(
-    db,
-    { useNewUrlParser: true },
-  )
-  .then(() => console.log('MongoDB successfully connected'))
-  .catch(err => console.log(err));
+// // Bodyparser middleware
+// app.use(
+//   bodyParser.urlencoded({
+//     extended: false,
+//   }),
+// );
+// app.use(multer({ dest: './uploads' }).any());
+// // app.use(fileUpload());
+// app.use(bodyParser.json());
+// // DB Config
+// const db = process.env.MONGODB_URI;
+// console.log(db);
+// // Connect to MongoDB
+// mongoose
+//   .connect(
+//     db,
+//     { useNewUrlParser: true },
+//   )
+//   .then(() => console.log('MongoDB successfully connected'))
+//   .catch(err => console.log(err));
 
-// Passport middleware
-app.use(passport.initialize());
-// Passport config
-require('./config/passport')(passport);
-// Routes
-app.use('/api/users', users);
+// // Passport middleware
+// app.use(passport.initialize());
+// // Passport config
+// require('./config/passport')(passport);
+// // Routes
+// app.use('/api/users', users);
 
-const port = process.env.PORT || 5000; // process.env.port is Heroku's port
+// const port = process.env.PORT || 5000; // process.env.port is Heroku's port
 
-// if you choose to deploy the app there
-app.listen(port, () => console.log(`Server up and running on port ${port} !`));
+// // if you choose to deploy the app there
+// app.listen(port, () => console.log(`Server up and running on port ${port} !`));
