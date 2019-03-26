47d283ea74b33efbf5810ae4b32165a179f716c9
Bogdan5 <bogdan5x@gmail.com>
2019-03-25 21:12:55 -0400

    Save file data in the database

--- routes/api/upload-csv.js
+++ routes/api/upload-csv.js
@@ -25,25 +25,36 @@ const User = require('../../models/User');
 router.post('/', authenticate.verifyUser, upload.single('file'),
   function (req, res) {
     console.log('res is: ', req.user._id);
-    // const fileRows = [];
-    // const decoded = jwt.verify(res.data.token, config.secretKey);
-    // console.log('decoded', decoded);
+    const fileRows = [];
+    const decoded = jwt.verify(res.data.token, config.secretKey);
+    console.log('decoded', decoded);
 
-    // // open uploaded file
-    // csv.fromPath(req.file.path)
-    //   .on("data", function (data) {
-    //     fileRows.push(data); // push each row
-    //   })
-    //   .on("end", async function () {
-    //     let header = [];
-    //     await fs.unlinkSync(req.file.path);   // remove temp file
-    //     //process "fileRows" and respond
-    //     if (req.body.firstRowHeader) {
-    //       header = fileRows.shift();
-    //     }
-    //     return res.sendStatus(200).end();
-    //   })
-    //   .on('error', (err) => res.sendStatus(404).end('Error in file upload: ', err));
+    // open uploaded file
+    csv.fromPath(req.file.path)
+      .on("data", function (data) {
+        fileRows.push(data); // push each row
+      })
+      .on("end", function () {
+        let header = [];
+        fs.unlinkSync(req.file.path);   // remove temp file
+        //process "fileRows" and respond
+        if (req.body.firstRowHeader) {
+          header = fileRows.shift();
+        }
+        User.update(
+          {'_id': ObjectId(req.user._id)},
+          {
+            '$push': {
+              'files': {
+                'header': header,
+                'data': fileRows
+              }
+            }
+          }
+        )
+        return res.sendStatus(200).end();
+      })
+      .on('error', (err) => res.sendStatus(404).end('Error in file upload: ', err));
 });
 
 
